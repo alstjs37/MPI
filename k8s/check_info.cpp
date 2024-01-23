506b5da0b548b35da89c8f39c971c17ad0d9818a
alstjs37 <minsun987654321@gmail.com>
2024-01-23 16:30:51 +0900

    functionalize

--- k8s/check_info.cpp
+++ k8s/check_info.cpp
@@ -3,7 +3,38 @@
 #include <arpa/inet.h>
 #include <fstream>
 
+#define num_of_node 2
+
 using namespace std;
+
+string domain_to_ip(const char* server_name) {
+    struct hostent *host;
+
+    string ip_addr, server_info;
+
+    while(1) {
+        host = gethostbyname(server_name);
+        if (host) {
+            ip_addr = inet_ntoa(*(struct in_addr*)host->h_addr_list[0]);
+            server_info = ip_addr + " " + server_name;
+
+            return server_info;
+        }
+        else cout << "[WARN] WAITING FOR CHECKING ... " << endl;
+    }
+}
+
+void write_file(string filename, string server_info) {
+    ofstream outfile(filename, ios::app);
+
+    if(outfile.is_open()) {
+        outfile << server_info << endl;
+        outfile.close();
+        cout << "[SUCCESS] FILE WRITE SUCCESS" << endl;
+    } else {
+        cout << "[ERROR] FAIL TO OPEN FILE" << endl;
+    }
+}
 
 int main (int argc, char *argv[]) {
 
@@ -16,30 +47,12 @@ int main (int argc, char *argv[]) {
     // pod_hostname.service_name.namespace (using k8s headless service)
     const char* server_name[2] = {"mpi-sn03.mpi-service.mpi-mslee", "mpi-sn04.mpi-service.mpi-mslee"};
     
-    for(int i = 0; i < 2; i++) {
-        if ((host = gethostbyname(server_name[i])) == NULL) {
-            cout << "ERROR: NOT DEFINE HOSTNAME ( " << server_name[i] << " )" << endl;
-        }
-
-        ip_addr = inet_ntoa(*(struct in_addr*)host->h_addr_list[i]);
-        server_info = ip_addr + " " + server_name[i];
-
-        // open file with write mode & write with append mode
-        ofstream outfile(filename, ios::app);
-
-        // if file open success
-        if (outfile.is_open()) {
-            // write at file
-            outfile << server_info << endl;
-
-            // close file
-            outfile.close();
-            cout << "FILE WRITE SUCCESS" << endl;
-        } else {
-            // fail to open file
-            cerr << "ERROR: FAIL TO OPEN FILE" << endl;
-        }
+    for(int i = 0; i < num_of_node; i++) {
+        server_info = domain_to_ip(server_name[i]);
+        write_file(filename, server_info);
     }
+
+    cout << "[SUCCESS] TOTAL PROCESS IS SUCCESSED !!" << endl;
 
     return 0;
 }
