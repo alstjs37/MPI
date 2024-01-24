ade469a0d70d64916720eb86888eac56a3949875
alstjs37 <minsun987654321@gmail.com>
2024-01-24 17:12:43 +0900

    add comment

--- info_init.sh
+++ info_init.sh
@@ -4,3 +4,5 @@ sudo g++ -o /root/mpiuser/k8s/check_info.out /root/mpiuser/k8s/check_info.cpp
 sudo /root/mpiuser/k8s/check_info.out
 
 source /root/mpiuser/key.sh
+
+echo "[SUCCESS] SSH KEY GENERATED && COPY TO OTHER PODS"
