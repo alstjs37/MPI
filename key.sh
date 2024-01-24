dc2d21e7a1743506d7c7758ab3614fb439fcb6e1
alstjs37 <minsun987654321@gmail.com>
2024-01-24 15:57:04 +0900

    key-copy-id automatically

--- key.sh
+++ key.sh
@@ -1,4 +1,22 @@
 # !/bin/bash
+HOST="mpi-sn04"
+PASSWORD="1234"
+
 ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa
 
-ssh-copy-id -i ~/.ssh/id_rsa.pub mpi-sn04.mpi-service.mpi-mslee
+expect << EOF
+    spawn ssh-copy-id $HOST
+    expect {
+        "yes/no" {
+            send "yes\r"
+            exp_continue
+        }
+        -nocase "password" {
+            send "$PASSWORD\r"
+            exp_continue
+        }
+        eof
+    }
+EOF
+
+# ssh-copy-id -i ~/.ssh/id_rsa.pub mpi-sn04
