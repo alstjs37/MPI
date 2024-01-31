16f42b458271ae3f5b754d7f57a19a820ff67932
alstjs37 <minsun987654321@gmail.com>
2024-01-31 19:04:11 +0900

    add print ( )

--- prac/hello_mpi.cpp
+++ prac/hello_mpi.cpp
@@ -8,8 +8,8 @@ int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
 
     // 현재 프로세스의 랭크와 전체 프로세스 수 얻기
-    int world_rank, world_size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    int rank, world_size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
     // 현재 프로세서의 이름을 저장할 배열
@@ -20,7 +20,7 @@ int main(int argc, char** argv) {
     MPI_Get_processor_name(processor_name, &node_name);
 
     // 각 프로세스에서 출력
-    cout << "Hello from process " << world_rank << " of " << world_size << " on node ( " << processor_name << " )" << endl;
+    cout << "Hello from process " << rank << " of " << world_size << " on node ( " << processor_name << " )" << endl;
 
     // MPI 종료
     MPI_Finalize();
